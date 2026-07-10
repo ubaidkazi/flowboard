2a2f3a7c7583c31d73daa54f051f641e30db467d
Ubaid <112728590+ubaidkazi@users.noreply.github.com>
2026-07-10 09:10:53 -0500

    feat(ui): update application sidebar
    update application sidebar

--- Frontend/flowboard-react-app/src/Components/Sidebar.jsx
+++ Frontend/flowboard-react-app/src/Components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import styles from '../styles/Sidebar.module.css';
 import { useNavigate } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import { Kanban, LayoutDashboard, FolderOpen, Users, ChartColumn, Settings, Cog, LogOut } from 'lucide-react';
+import { Kanban, LayoutDashboard, FolderOpen, FolderKanban, Users, ChartColumn, Settings, Cog, LogOut } from 'lucide-react';
+import Avatar from './ui/avatar-new';
 
 function Sidebar()
 {
@@ -9,6 +10,7 @@ function Sidebar()
 
     const fullName = localStorage.getItem("fullName");
     const email = localStorage.getItem("userEmail");
+    const userId = localStorage.getItem("userId");
     
 
     
@@ -25,47 +27,41 @@ function Sidebar()
     if (!fullName) return '';
 
     const names = fullName.trim().split(' ');
+    console.log(names);
     if (names.length === 1) return names[0][0].toUpperCase();
+
+    if(names.length > 2 ) return (
+    names[0][0].toUpperCase() + names[1][0].toUpperCase()
+    );
 
     return (
     names[0][0].toUpperCase() + names[names.length - 1][0].toUpperCase()
+    
     );
     }
 
-
     const initials  = getInitials(fullName);
 
+    localStorage.setItem("userInitials", initials);
 
+    
 
 
     return(
         <>
 
-        <div className={styles['sidebar']}>
-            <div className={styles['sidebar-header']} >
-                <div className={styles["brand-name-full"]}>
-                    <Kanban color="#1462df" strokeWidth={2}  className={styles['icon']}/>
-                    <NavLink to='/' className={styles['brand-name']}> FlowBoard</NavLink>
-                </div>
-                <div className={styles['divider']} />
-
-            </div>
-
-            
-
-
-            
-            
+        <div className={`${styles.sidebar} fixed pt-16 `}>
             
         
             <div className={styles["sidebar-links"]} >
 
                 <div className={styles["top-section"]}>
-                    <NavLink to="/newdashboard" className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]} > <LayoutDashboard size={20} strokeWidth={1.5} className={styles["link-icon"]} /> Dashboard </NavLink>
-                    <NavLink to="/Dashboard/Projects" className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}> <FolderOpen size={20}  strokeWidth={1.5}  className={styles["link-icon"]}/> Projects </NavLink>
-                    <NavLink to="/Dashboard/people"className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}>  <Users size={20} strokeWidth={1.5} className={styles["link-icon"]} /> My Team </NavLink>
-                    <NavLink to="/Dashboard/Analytics" className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}>  <ChartColumn size={20}  strokeWidth={1.5} className={styles["link-icon"]} /> Analytics </NavLink>
-                    <NavLink to="/Dashboard/Settings"className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}><Settings size={20} strokeWidth={1.5} className={styles["link-icon"]} /> Settings </NavLink>
+                    <NavLink to="/dashboard" end  className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]} > <LayoutDashboard size={22} strokeWidth={1.5} className={styles["link-icon"]} /> Dashboard </NavLink>
+                    <NavLink to="/Dashboard/Projects" className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}> <FolderKanban size={22}  strokeWidth={1.5}  className={styles["link-icon"]}/> Projects </NavLink>
+                    {/* <NavLink to="/Dashboard/people"className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}> <Users size={22} strokeWidth={1.5} className={styles["link-icon"]}  /> <span> My Team </span>  </NavLink> */}
+                    <NavLink to="/Dashboard/Analytics" className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}>  <ChartColumn size={22}  strokeWidth={1.5} className={styles["link-icon"]} /> Analytics </NavLink>
+                    <NavLink to="/Dashboard/Settings"className={({ isActive }) => isActive  ? `${styles["nav-link"]} ${styles["active-nav-link"]}` : styles["nav-link"]}><Settings size={22} strokeWidth={1.5} className={styles["link-icon"]} /> Settings </NavLink>
+                
                 </div>
 
 
@@ -75,9 +71,7 @@ function Sidebar()
                 {/* <NavLink to="/dashboard/settings" className={styles["nav-link"]}>   <Users size={25} color="#1a1c1def" strokeWidth={1.5} className={styles["link-icon"]} /> Name section </NavLink> */}
                 <div className={styles["user-detail-div"]}>
                     <div className={styles["profile-card"]}>
-                        <div className={styles["profile-avatar"]}>
-                        {initials}
-                        </div>
+                         <Avatar fullName={fullName} userId={userId}></Avatar>
                                             
                     </div>
                     <div className={styles["name-email-div"]}>
@@ -86,16 +80,17 @@ function Sidebar()
                     </div>
                     
 
-
                 </div>
+
+                
+
+               
                 
                 <button onClick={handleLogout} className={styles["logout-button"]}> <div className={styles["logout-icon-div"]}> <LogOut size={20}> </LogOut>  Logout  </div>  </button>
                 </div>
 
-            {/* {localStorage.getItem("fullName")}  {localStorage.getItem("userEmail")} */}
-
             </div>
-        
+
         </div>
         
         </>
